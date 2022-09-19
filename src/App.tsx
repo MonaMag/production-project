2a0fabd0c988b0ff4b091231c7cefee3da0f5025
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2022-09-20 00:22:23 +0300

    S8. Add helper classNames.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, {Suspense, useContext, useState} from 'react';
+import React, {Suspense} from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
 import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 
 const App = () => {
@@ -11,7 +12,7 @@ const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
