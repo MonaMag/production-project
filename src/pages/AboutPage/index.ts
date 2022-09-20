3b64eca04f64cfd82495a8c72f6e83ddd3ec9763
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2022-09-20 16:08:07 +0300

    S9-10. Build clear structure of folders (use FSD methodology) and configure absolute imports.

---
+++ src/pages/AboutPage/index.ts
@@ -0,0 +1,7 @@
+import {AboutPageAsync} from "./ui/AboutPage.async";
+
+export {
+    AboutPageAsync as AboutPage,
+}
+//можно сразу сделать экспор и импорт из нужного места
+//export {AboutPageAsync} from './ui/AboutPage';
---
+++ src/pages/MainPage/index.ts
@@ -0,0 +1,8 @@
+import {MainPageAsync} from "./ui/MainPage.async";
+
+export {
+    MainPageAsync as MainPage,
+}
+
+//export {MainPageAsync} from './ui/MainPage.async';
+
