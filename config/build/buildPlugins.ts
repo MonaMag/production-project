d0b1412fec28cf82d463e97b49d9e7444c164484
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2022-09-25 01:02:14 +0300

    S16. Add HotModuleReplacementPlugin.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,4 +1,3 @@
-
 import webpack from "webpack";
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import {BuildOptions} from "./types/config";
@@ -6,18 +5,19 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
 
-  return [
-    new HTMLWebpackPlugin({
-      template: paths.html,
-    }),
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css',
-    }),
-      //__IS_DEV__ так называем глоб переменные сборки, чтобы четко их отделять от переменных
-      new webpack.DefinePlugin({
-        __IS_DEV__: JSON.stringify(isDev),
-      })
-  ]
+    return [
+        new HTMLWebpackPlugin({
+            template: paths.html,
+        }),
+        new webpack.ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css',
+        }),
+        //__IS_DEV__ так называем глоб переменные сборки, чтобы четко их отделять от переменных
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ]
 }
