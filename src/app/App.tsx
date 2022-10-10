8a11b129e30b45686f1785dc4d36ffa9ed5a4ae3
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2022-10-10 13:47:49 +0300

    S29. Add simple modal window.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,24 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Modal } from 'shared/ui/Modal/Modal';
 import AppRouter from './providers/router/ui/AppRouter';
 
 const App = () => {
     const { theme } = useTheme();
 
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
+                <button onClick={() => setIsOpen(true)}>toggle</button>
+                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci doloremque dolorum explicabo incidunt labore numquam provident rem repellendus ullam velit?
+                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
