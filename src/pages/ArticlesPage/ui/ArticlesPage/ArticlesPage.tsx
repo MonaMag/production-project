c86694245479768a9ff6b954a563fd411f5d5a73
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2022-11-17 22:50:39 +0300

    S59. Add filters, sort and search

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,19 +1,21 @@
 import React, { memo, useCallback } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { ArticleList, ArticleView, ArticleViewSelector } from 'entities/Article';
+import { ArticleList } from 'entities/Article';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 import { useSelector } from 'react-redux';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
 import { Page } from 'widgets/Page/Page';
 import { initArticlesPage } from 'pages/ArticlesPage/modal/services/initArticlePage/initArticlePage';
+import { useSearchParams } from 'react-router-dom';
+import { ArticlesPageFilters } from 'pages/ArticlesPage/ui/ArticlesPageFilters/ArticlesPageFilters';
 import {
     getArticlesPageError, getArticlesPageInited,
     getArticlesPageIsLoading,
     getArticlesPageView,
 } from '../../modal/selectors/articlesPageSelectors';
-import { articlesPageActions, articlesPageReducer, getArticles } from '../../modal/slices/articlesPageSlice';
+import { articlesPageReducer, getArticles } from '../../modal/slices/articlesPageSlice';
 import cls from './ArticlesPage.module.scss';
 import { fetchNextArticlesPage } from '../../modal/services/fetchNextArticlesPage/fetchNextArticlesPage';
 
@@ -35,19 +37,16 @@ const ArticlesPage = (props: ArticlesPageProps) => {
     const view = useSelector(getArticlesPageView);
     const error = useSelector(getArticlesPageError);
     const inited = useSelector(getArticlesPageInited);
+    const [searchParams] = useSearchParams();
 
     const { t } = useTranslation('article-details');
-
-    const onChangeView = useCallback((view: ArticleView) => {
-        dispatch(articlesPageActions.setView(view));
-    }, [dispatch]);
 
     const onLoadNextPart = useCallback(() => {
         dispatch(fetchNextArticlesPage());
     }, [dispatch]);
 
     useInitialEffect(() => {
-        dispatch(initArticlesPage());
+        dispatch(initArticlesPage(searchParams));
     });
 
     return (
@@ -56,11 +55,12 @@ const ArticlesPage = (props: ArticlesPageProps) => {
                 onScrollEnd={onLoadNextPart}
                 className={classNames(cls.ArticlesPage, {}, [className])}
             >
-                <ArticleViewSelector view={view} onViewClick={onChangeView} />
+                <ArticlesPageFilters />
                 <ArticleList
                     isLoading={isLoading}
                     view={view}
                     articles={articles}
+                    className={cls.list}
                 />
             </Page>
         </DynamicModuleLoader>
