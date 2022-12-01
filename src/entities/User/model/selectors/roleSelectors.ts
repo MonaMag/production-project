6ff4f625d6be9360ab6d19fd8d94cc4f4fdb786a
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2022-12-01 14:21:17 +0300

    S73. Add private route with required roles.

---
+++ src/entities/User/model/selectors/roleSelectors.ts
@@ -0,0 +1,11 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import { createSelector } from '@reduxjs/toolkit';
+import { UserRole } from '../types/user';
+
+// в этом селекторе достаю весь массив ролей
+export const getUserRoles = (state: StateSchema) => state.user.authData?.roles;
+// Зачастую проверяем является ли пользователь админом или менеджером
+// и чтобы каждый раз не пробегать по массиву ролей и не проверять,
+// с помощью реселект мемоизируем получение списка ролей и уже внутри селектора проверяем роль пользователя
+export const isUserAdmin = createSelector(getUserRoles, (roles) => Boolean(roles?.includes(UserRole.ADMIN)));
+export const isUserManager = createSelector(getUserRoles, (roles) => Boolean(roles?.includes(UserRole.MANAGER)));
